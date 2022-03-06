eed24102dcab11dc11b2a4f4b6c5ea70540a2012
andy <chuxin135@163.com>
2022-03-06 22:30:48 +0800

    baisc home

--- yyyy/src/main/java/com/example/demo/ZzzzApplication.java
+++ yyyy/src/main/java/com/example/demo/ZzzzApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.sql.SQLOutput;
 
+import java.util.Arrays;
+
 //@SpringBootApplication
 public class ZzzzApplication {
 
@@ -87,6 +89,95 @@ public class ZzzzApplication {
         System.out.println("d3的值：" + d3);
         System.out.println("d4的值：" + d4);
         System.out.println("abs函数：" + Math.abs(1 - 3));
+
+        Person p = new Person();
+        p.getAge();
     }
 
+}
+
+class Person {
+    private int age;
+
+    public void getAge() {
+
+        // switch 语句
+        String fruit = "apple";
+        switch (fruit) {
+            case "apple":
+                System.out.println("select apple");
+                break;
+            case "33":
+                System.out.println("select 33");
+                break;
+            default:
+                System.out.println("select default");
+                break;
+        }
+
+        // while 语句
+        int sum = 0;
+        int n = 0;
+        while (n <= 99) {
+            n++;
+            sum += n;
+        }
+        System.out.println("sum的值：" + sum);
+
+        sum = 0;
+        n = 1;
+        do {
+            sum += n;
+            n++;
+        } while (n <= 100);
+        System.out.println("sum的值：" + sum);
+
+        // for 循环
+        int nsArr[] = {1, 3 ,5 ,7, 0};
+        sum = 0;
+        for (int i = 0; i < nsArr.length; i++) {
+            sum += nsArr[i];
+        }
+        System.out.println("sum的值：" + sum);
+
+        for(int j : nsArr) {
+            System.out.println("遍历数组的值：" + j);
+        }
+        // break 使用
+        for (int i=1; i<4; i++) {
+            System.out.println("i的值：" + i);
+            for (int j=1; ;j++) {
+                if (j == 10) {
+                    System.out.println("j的值:" + j);
+                    break;
+                }
+            }
+        }
+
+        // 数组
+        char ssArr[] = {'a', 'c', 'e', 'g', 'h'};
+        for (char k : ssArr) {
+            System.out.println("k的值：" + k);
+        }
+        System.out.println("ssArr数组在JVM中的引用地址：" + ssArr);
+        System.out.println("ssArr数组内容：" + Arrays.toString(ssArr));
+        // 数组排序
+        int ttArr[] = {8, 4, 1, 5, 7, 6};
+        Arrays.sort(ttArr);
+        System.out.println("ttArr数组排序后的内容：" + Arrays.toString(ttArr));
+        if (Arrays.toString(ttArr).equals("[1, 4, 5, 6, 7, 8]")) {
+            System.out.println("判断字符串相等与否, Arrays.toString(ttArr).equals(\"[1, 4, 5, 6, 7, 8]\")");
+        }
+        // 二维数组
+        int[][] twoDimenArr = {
+                {15, 6, 7, 8},
+                {9, 20, 11, 12, 88},
+                {1, 3, 7, 0}
+        };
+        System.out.println("二维数组 twoDimenArr 的长度：" + twoDimenArr.length);
+        System.out.println("二维数组 twoDimenArr, 第二维的值：" + Arrays.toString(twoDimenArr[1]));
+        System.out.println("二维数组 twoDimenArr, 第二维的长度：" + twoDimenArr[1].length);
+        System.out.println("二维数组 twoDimenArr, 打印某个值：" + twoDimenArr[1][1]);
+
+    }
 }
