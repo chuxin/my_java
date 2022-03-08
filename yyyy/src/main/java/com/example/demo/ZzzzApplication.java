611f77225ee9939d6b29e0e584058aa15b3154dd
Qian Chao <Qian.Chao@worldfirst.com>
2022-03-08 21:55:00 +0800

    home oop

--- yyyy/src/main/java/com/example/demo/ZzzzApplication.java
+++ yyyy/src/main/java/com/example/demo/ZzzzApplication.java
@@ -13,8 +13,16 @@ public class ZzzzApplication {
 //        BasicKnowledge bkObj = new BasicKnowledge();
 //        bkObj.testBasic(myArgs);
 
-
+        // 构造方法
         OOPknowledge mtObj = new OOPknowledge();
+        System.out.println("第一个构造函数传入的值：" + mtObj.val11 + " " + mtObj.age11);
+        OOPknowledge mtObj11 = new OOPknowledge("bbb", 33);
+        System.out.println("第二个构造函数传入的值：" + mtObj11.val11 + " " + mtObj11.age11);
+        OOPknowledge mtObj22 = new OOPknowledge("ccc");
+        System.out.println("第三个构造函数传入的值：" + mtObj22.val22 + " " + mtObj22.val11
+                + " " + mtObj22.age11);
+
+        // 方法
         mtObj.name = "louis qian";
         mtObj.age = -100;
 
@@ -26,9 +34,28 @@ public class ZzzzApplication {
         mtObj.getFormerNames("aa", "bb", "xxx", "yyy");
         mtObj.getFormerNames("aa");
         mtObj.getFormerNames();
+        mtObj.getFormerNames(null);
 
         mtObj.getFormerAddresses(new String[] {"mm", "nn", "pp"});
         mtObj.getFormerAddresses(new String[] {});
+        mtObj.getFormerAddresses(null);
+
+        // 方法重载
+        String s = "Test string";
+        int n1 = s.indexOf("t");
+        int n2 = s.indexOf("es");
+        int n3 = s.indexOf("t", 4);
+        System.out.println("indexOf函数，传入不同参数，返回的位置分别是：" + n1 + ", " + n2 + ", " + n3);
+
+        OOPknowledge myObjLoad11 = new OOPknowledge();
+        OOPknowledge myObjLoad22 = new OOPknowledge();
+        myObjLoad11.setCountry("test overload 11");
+        myObjLoad22.setCountry("test overload 22", "test overload 22-22");
+        System.out.println("重载方法，调用定义了一个参数的setCountry：" + myObjLoad11.country + ", " + myObjLoad11.secondCountry);
+        System.out.println("重载方法，调用定义了两个参数的setCountry：" + myObjLoad22.country + ", " + myObjLoad22.secondCountry);
+
+        // 继承
+        
     }
 
 }
--- yyyy/src/main/java/com/example/demo/mypackages/OOPknowledge.java
+++ yyyy/src/main/java/com/example/demo/mypackages/OOPknowledge.java
@@ -3,6 +3,11 @@ package com.example.demo.mypackages;
 import java.util.Arrays;
 
 public class OOPknowledge {
+    public String val11;
+    public int age11;
+
+    public String val22;
+
     public String name;
     public int age;
 
@@ -11,6 +16,27 @@ public class OOPknowledge {
 
     public String[] formerNames;
     public String[] formerAddresses;
+
+    public String country;
+    public String secondCountry;
+
+    // 第一个构造函数
+    public OOPknowledge() {
+        // 什么都没有
+    }
+
+    // 第二个构造函数
+    public OOPknowledge(String val11, int age11) {
+        this.val11 = val11;
+        this.age11 = age11;
+    }
+
+    // 第三个构造函数
+    public OOPknowledge(String val22) {
+        // 这里第三个构造函数 去 调用第二个构造方法
+        this("ccc22", 44);
+        this.val22 = val22;
+    }
 
     public void setAddress(String address) {
         this.address = address;
@@ -45,5 +71,14 @@ public class OOPknowledge {
     public void getFormerAddresses(String[] formerAddresses) {
         this.formerAddresses = formerAddresses;
         System.out.println("曾用地址：" + Arrays.toString(this.formerAddresses));
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public void setCountry(String country, String secondCountry) {
+        this.country = country;
+        this.secondCountry = secondCountry;
     }
 }
